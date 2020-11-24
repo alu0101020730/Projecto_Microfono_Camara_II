cb01a56e9925ac1c1675f2a423da3842480b98af
Cristian Rodriguez <43813175+alu0101020730@users.noreply.github.com>
2020-11-24 13:11:22 +0000

    Add files via upload

---
+++ Cam.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cam : MonoBehaviour
+{
+    public RawImage rawimage;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        WebCamTexture webcamTexture = new WebCamTexture();
+        rawimage.texture = webcamTexture;
+        rawimage.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Microph.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Microph : MonoBehaviour
+{
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+       audioSource = GetComponent<AudioSource>();
+        
+    }
+
+    public void Record()
+    {
+        audioSource.clip = Microphone.Start(null, false, 10, 44100);
+    }
+
+    public void Play()
+    {
+        audioSource.Play();
+    }
+
+    public void EndRecording()
+    {
+        Microphone.End(null);
+    }
+}
